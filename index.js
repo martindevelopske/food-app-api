14eeef60ba412f09b2bc2401f89c6062f33e3e55
Martin Ndung'u <martindevelopske@gmail.com>
2024-05-27 16:26:05 +0300

    fixed import issues

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const colors = require("colors");
 const cors = require("cors");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
-const connectDb = require("./config/db");
+const connectDb = require("./src/config/db.js");
 
 //dot en configuration
 dotenv.config();
@@ -21,12 +21,12 @@ app.use(morgan("dev"));
 
 //route
 // URL => http://localhost:8080
-app.use("/api/v1/test", require("./routes/testRoutes"));
-app.use("/api/v1/auth", require("./routes/authRoutes"));
-app.use("/api/v1/user", require("./routes/userRoutes"));
-app.use("/api/v1/resturant", require("./routes/resturantRoutes"));
-app.use("/api/v1/category", require("./routes/catgeoryRoutes"));
-app.use("/api/v1/food", require("./routes/foodRoutes"));
+app.use("/api/v1/test", require("./src/routes/testRoutes"));
+app.use("/api/v1/auth", require("./src/routes/authRoutes"));
+app.use("/api/v1/user", require("./src/routes/userRoutes"));
+app.use("/api/v1/resturant", require("./src/routes/resturantRoutes"));
+app.use("/api/v1/category", require("./src/routes/catgeoryRoutes"));
+app.use("/api/v1/food", require("./src/routes/foodRoutes"));
 
 app.get("/", (req, res) => {
   return res
